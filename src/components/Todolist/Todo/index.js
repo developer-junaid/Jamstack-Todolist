44f0ad7d004f8d5517d6b0540b1d828ac61fdf2b
developer-junaid <qjunaid623@gmail.com>
2021-08-04 20:47:11 +0500

    made site responsive

--- src/components/Todolist/Todo/index.js
+++ src/components/Todolist/Todo/index.js
@@ -1,5 +1,10 @@
 import React, { useState, useRef } from "react"
 
+// icons
+import deleteIcon from "./../../../../static/garbage-can.svg"
+import editIcon from "./../../../../static/pencil.svg"
+import doneIcon from "./../../../../static/done.svg"
+
 // Todo
 const Todo = ({ task, refreshTodos }) => {
   const [editing, setEditing] = useState(false)
@@ -47,29 +52,29 @@ const Todo = ({ task, refreshTodos }) => {
   const editButton = editing ? (
     <a
       onClick={updateTodo}
-      className="text-indigo-600 text-base p-2 hover:text-indigo-900 cursor-pointer"
+      className="text-indigo-600 text-base p-1 hover:text-indigo-900 cursor-pointer"
     >
-      Done
+      <img src={doneIcon} alt="done" className="min-w-5 h-5 inline" />
     </a>
   ) : (
     <a
       onClick={handleEdit}
-      className="text-indigo-600 text-base p-2 hover:text-indigo-900 cursor-pointer"
+      className="text-indigo-600 text-base p-1 hover:text-indigo-900 cursor-pointer"
     >
-      Edit
+      <img src={editIcon} alt="edit" className="min-w-5 h-5 inline" />{" "}
     </a>
   )
 
   const todoText = editing ? (
     <input
-      className="text-base p-2 focus:ring focus:border-blue-300 font-medium outline-none text-gray-900"
+      className="text-sm text w-full px-2 ml-2 font-medium  focus:ring focus:border-blue-300 overflow-scroll  font-medium outline-none text-gray-900"
       value={taskToUpdate.text}
       onChange={handleChange}
       ref={inputEl}
     />
   ) : (
     <input
-      className="text-base p-2 cursor-default font-medium outline-none text-gray-900"
+      className="text-sm px-2 w-full  ml-2 font-mediumcursor-default font-medium overflow-scroll overflow-scroll  outline-none text-gray-900"
       value={taskToUpdate.text}
       onChange={handleChange}
       readOnly
@@ -80,30 +85,39 @@ const Todo = ({ task, refreshTodos }) => {
   // Return
   return (
     <div className="flex flex-col">
-      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table className="min-w-full divide-y divide-gray-200">
-              <tbody className="bg-white divide-y divide-gray-200">
-                <tr>
-                  <td className="px-2 py-4 whitespace-nowrap">
-                    <div className="flex items-center">
-                      <div className="ml-4">{todoText}</div>
-                    </div>
-                  </td>
-
-                  <td className="px-6 py-4 whitespace-nowrap flex items-center justify-end text-right text-sm font-medium">
+      <div className="-my-2 flex align-center sm:-mx-6 lg:-mx-8 ">
+        <div className="py-2 align-middle inline-block lg:w-2/3 mx-auto  w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden  border-b border-gray-200 sm:rounded-lg">
+            <div className="min-w-5/6  divide-y divide-gray-200">
+              <div className="bg-white divide-y divide-gray-200">
+                {/* Container */}
+                <div className="px-2 min-w-full py-4 flex justify-between align-center ">
+                  {/* Input */}
+                  {/* <div className="flex-row min-w-1/3 w-1/3 border-2 border-black inline-block items-center"> */}
+                  {/* <div className="ml-4 font-medium text-lg "></div> */}
+                  {/* </div> */}
+                  {todoText}
+                  {/* Buttons */}
+                  <div className="inline-block flex justify-end items-center min-w-48 w-36 ">
                     {editButton}
                     <a
                       onClick={deleteTodo}
-                      className="text-indigo-600 text-base p-2 hover:text-indigo-900 ml-4 cursor-pointer"
+                      className="text-indigo-600 p-2 hover:text-indigo-900 ml-2 cursor-pointer"
                     >
-                      Delete
+                      <img
+                        src={deleteIcon}
+                        alt="delete"
+                        className="min-w-5 h-5 inline"
+                      />
                     </a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+                  </div>
+
+                  {/* <div className="px-3 min-w-1/3 inline-block border-2 border-green-800 py-4  flex items-center justify-end text-right text-sm font-medium">
+                   
+                  </div> */}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
